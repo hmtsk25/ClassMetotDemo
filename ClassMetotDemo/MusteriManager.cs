73ee1eff2bb31560a7d0f80f5d41d1f13c666b9c
1n0 <19034hm@gmail.com>
2021-01-17 21:25:46 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} isimli müşteri eklendi!",musteri.Ad,musteri.Soyad);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} isimli müşteri silindi!", musteri.Ad, musteri.Soyad);
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            Console.WriteLine("\nMÜŞTERİLER");
+            Console.WriteLine("-----------\n");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine($"Müşteri adı soyadı: {musteri.Ad} {musteri.Soyad}, Yaşı: {musteri.Yas}, Kullanılabilir Hesap Bakiyesi: {musteri.HesapBakiyesi} TL ");
+                Console.WriteLine(" -----------------------" );
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 103;
+            musteri1.Ad = "Salih";
+            musteri1.Soyad = "Kaplan";
+            musteri1.Yas = 32;
+            musteri1.HesapBakiyesi = 1_500;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 104;
+            musteri2.Ad = "Buse";
+            musteri2.Soyad = "Yıldız";
+            musteri2.Yas = 27;
+            musteri2.HesapBakiyesi = 3_500;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteriler);
+
+
+
+
+            Console.Read();
+        }
+    }
+}
